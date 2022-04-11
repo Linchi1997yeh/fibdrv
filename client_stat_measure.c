9b5d3698c8b0b62833dd3f43fee66730bdc2614c
Linchi <linchi1997yeh@gmail.com>
2022-04-11 10:50:20 +0800

    Fix GNU plot

--- client_stat_measure.c
+++ client_stat_measure.c
@@ -21,35 +21,29 @@ int main()
     }
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        double kernel_time[iterations];
-        double user_time[iterations];
-        double mean_k = 0.0, sd_k = 0.0, sd_u = 0.0, mean_u = 0.0, res_k = 0.0,
-               res_u = 0.0, sys_cal = 0.0;
-        int count_k = 0, count_u = 0;
+        double time_1[iterations];
+        double time_2[iterations];
+        double mean_t1 = 0.0, sd_k = 0.0, sd_u = 0.0, mean_t2 = 0.0,
+               res_t1 = 0.0, res_t2 = 0.0, time_dif = 0.0;
+        int count_t1 = 0, count_t2 = 0;
+        // Choose 2 metrics (t1, t2) to be calculated
         for (int j = 0; j < iterations; j++) {
-            long long sz;
-            // long long fz;
-            struct timespec t_start, t_end;
-            clock_gettime(CLOCK_REALTIME, &t_start);
-            sz = write(fd, write_buf, 0);
-            // fz = write(fd, write_buf, 1);
-            clock_gettime(CLOCK_REALTIME, &t_end);
-            long long dif = t_end.tv_nsec - t_start.tv_nsec;
-            kernel_time[j] = sz;
-            // user_time[j] = fz;
-            user_time[j] = dif;
-            mean_k += sz;
-            mean_u += dif;
-            // mean_u += dif;
+            long long t1, t2;
+            t2 = write(fd, write_buf, 0);
+            t1 = write(fd, write_buf, 1);
+            time_1[j] = t1;
+            time_2[j] = t2;
+            mean_t1 += t1;
+            mean_t2 += t2;
         }
         // get mean
-        mean_k /= iterations;
-        mean_u /= iterations;
+        mean_t1 /= iterations;
+        mean_t2 /= iterations;
 
         // get standar deviation
         for (int j = 0; j < iterations; j++) {
-            sd_k += (kernel_time[j] - mean_k) * (kernel_time[j] - mean_k);
-            sd_u += (user_time[j] - mean_u) * (user_time[j] - mean_u);
+            sd_k += (time_1[j] - mean_t1) * (time_1[j] - mean_t1);
+            sd_u += (time_2[j] - mean_t2) * (time_2[j] - mean_t2);
         }
         sd_k = sqrt(sd_k / (iterations - 1));
         sd_u = sqrt(sd_u / (iterations - 1));
@@ -57,21 +51,21 @@ int main()
 
         // remove outliers
         for (int j = 0; j < iterations; j++) {
-            if (kernel_time[j] <= (mean_k + 2 * sd_k) &&
-                kernel_time[j] >= (mean_k - 2 * sd_k)) {
-                res_k += kernel_time[j];
-                count_k += 1;
+            if (time_1[j] <= (mean_t1 + 2 * sd_k) &&
+                time_1[j] >= (mean_t1 - 2 * sd_k)) {
+                res_t1 += time_1[j];
+                count_t1 += 1;
             }
-            if (user_time[j] <= (mean_u + 2 * sd_u) &&
-                user_time[j] >= (mean_u - 2 * sd_u)) {
-                res_u += user_time[j];
-                count_u += 1;
+            if (time_2[j] <= (mean_t2 + 2 * sd_u) &&
+                time_2[j] >= (mean_t2 - 2 * sd_u)) {
+                res_t2 += time_2[j];
+                count_t2 += 1;
             }
         }
-        res_k /= count_k;
-        res_u /= count_u;
-        sys_cal = res_u - res_k;
-        printf("%d %f %f %f \n", i, res_k, res_u, sys_cal);
+        res_t1 /= count_t1;
+        res_t2 /= count_t2;
+        time_dif = res_t2 - res_t1;
+        printf("%d %f %f %f \n", i, res_t1, res_t2, time_dif);
     }
 
     close(fd);
